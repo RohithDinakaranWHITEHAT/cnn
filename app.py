41351fb140be2f1cc5a8e1ef6ad4ece0fd53ec2d
RohithDinakaranWHITEHAT <61183902+RohithDinakaranWHITEHAT@users.noreply.github.com>
2023-11-16 18:29:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,6 +22,60 @@ model_summary = """
         </p>
     </div>
 """
+model = CNN(num_classes=36, dropout_rate=0.2)
+model.load_state_dict(torch.load("C:\\Users\\rohit\\Downloads\\rohithdi_ssnehaba_assignment2_part3.h5", map_location=torch.device('cpu')))
+model.eval()
+
+def preprocess_image(image):
+
+    image = Image.fromarray(image)
+    
+
+    transform = transforms.Compose([
+        transforms.Resize((28, 28)),
+        transforms.ToTensor(),
+    ])
+    
+    image = transform(image)
+    image = image.unsqueeze(0)  
+
+    return image
+
+
+label_mapping = {
+    0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5',
+    6: '6', 7: '7', 8: '8', 9: '9', 10: 'a', 11: 'b',
+    12: 'c', 13: 'd', 14: 'e', 15: 'f', 16: 'g', 17: 'h',
+    18: 'i', 19: 'j', 20: 'k', 21: 'l', 22: 'm', 23: 'n',
+    24: 'o', 25: 'p', 26: 'q', 27: 'r', 28: 's', 29: 't',
+    30: 'u', 31: 'v', 32: 'w', 33: 'x', 34: 'y', 35: 'z'
+}
+
+
+css_style = """
+    p.predicted-class {
+        font-size: 30px;
+        font-weight: bold;
+        animation: textZoom 1s ease;
+        text-align: center;
+    }
+
+    @keyframes textZoom {
+        from { font-size: 30px; }
+        to { font-size: 16px; }
+    }
+"""
+
+def predict(image):
+    with torch.no_grad():
+        output = model(image)
+
+    _, predicted_class = torch.max(output, 1)
+    predicted_class = int(predicted_class)
+
+    original_label = label_mapping.get(predicted_class, 'Unknown')
+
+    return original_label
 
 
 def main():
