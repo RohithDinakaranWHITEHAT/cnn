f9fc077bd882f477edc7f11baa806da846978852
RohithDinakaranWHITEHAT <61183902+RohithDinakaranWHITEHAT@users.noreply.github.com>
2023-11-16 18:37:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,35 +5,26 @@ import torch
 from torchvision import transforms
 from cnn import CNN  
 
+
 model = CNN(num_classes=36, dropout_rate=0.2)
 model.load_state_dict(torch.load("rohithdi_ssnehaba_assignment2_part3.h5", map_location=torch.device('cpu')))
 model.eval()
-model_summary = """
-    <div style="background-color: #f0f0f0; padding: 20px; border-radius: 10px; margin-bottom: 20px;">
-        <h3 style="color: #333;">Model Summary</h3>
-        <p style="color: #555;">
-            <strong>CNN Architecture:</strong><br>
-            This CNN model consists of five convolutional layers, each followed by a ReLU activation function and a max pooling layer.
-            The output of the convolutional layers is then flattened and passed through three fully connected layers, each followed by a ReLU activation function.
-            The final fully connected layer has a number of outputs equal to the number of classes in the dataset.
-        </p>
-        <p style="color: #555;">
-            <strong>Training Process:</strong><br>
-            The model was trained using early stopping for 400 epochs with a batch size of 16 and an Adagrad optimizer with a learning rate of 0.001.
-            The model achieved an accuracy of 89.12% on the test set.
-        </p>
-    </div>
-"""
 
 def preprocess_image(image):
+
     image = Image.fromarray(image)
+    
+
     transform = transforms.Compose([
         transforms.Resize((28, 28)),
         transforms.ToTensor(),
     ])
+    
     image = transform(image)
     image = image.unsqueeze(0)  
+
     return image
+
 
 label_mapping = {
     0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5',
@@ -44,12 +35,18 @@ label_mapping = {
     30: 'u', 31: 'v', 32: 'w', 33: 'x', 34: 'y', 35: 'z'
 }
 
+
 css_style = """
     p.predicted-class {
-        font-size: 24px;
+        font-size: 30px;
         font-weight: bold;
-        color: #3498db;
+        animation: textZoom 1s ease;
         text-align: center;
+    }
+
+    @keyframes textZoom {
+        from { font-size: 30px; }
+        to { font-size: 16px; }
     }
 """
 
@@ -67,15 +64,13 @@ def predict(image):
 def main():
     st.title("Image Classifier")
 
-    st.markdown(model_summary, unsafe_allow_html=True)
-
     uploaded_file = st.file_uploader("Choose an image...", type="png")
 
     st.markdown(f'<style>{css_style}</style>', unsafe_allow_html=True)
 
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded Image", use_column_width=True, style="border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);")
+        st.image(image, caption="Uploaded Image", use_column_width=True)
 
         try:
             processed_image = preprocess_image(np.array(image))
